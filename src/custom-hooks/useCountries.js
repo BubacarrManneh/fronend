46e07ddc22193bf2cecf36cb7d2928c12b5f80d8
BubacarrManneh <bubacarr.manneh@integrify.io>
2022-05-09 22:41:22 +0200

    Hooks and Pages

---
+++ src/custom-hooks/useCountries.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCountries } from "../Redux/action";
+
+const useCountries = (url) => {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => {
+    return state.countries;
+  });
+  const fetchError = useSelector((state) => {
+    return state.error;
+  });
+  useEffect(() => {
+    dispatch(getCountries);
+  }, [dispatch, url]);
+
+  return [fetchError, countries];
+};
+
+export default useCountries;
---
+++ src/custom-hooks/useCountry.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getOneCountry } from "../Redux/action";
+
+const useCountry = (url) => {
+  const dispatch = useDispatch();
+  const country = useSelector((state) => {
+    return state.country;
+  });
+  const fetchError = useSelector((state) => {
+    return state.error;
+  });
+  useEffect(() => {
+    dispatch(getOneCountry(country));
+  }, [country, dispatch, url]);
+
+  return [fetchError, country];
+};
+
+export default useCountry;
